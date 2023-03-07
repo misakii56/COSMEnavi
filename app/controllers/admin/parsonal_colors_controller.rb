735d27e2bb66ba62347918813862bf6d49553e95
EC2 Default User <ec2-user@ip-172-31-29-247.ap-northeast-1.compute.internal>
2023-03-07 11:06:56 +0000

    0307

--- app/controllers/admin/parsonal_colors_controller.rb
+++ app/controllers/admin/parsonal_colors_controller.rb
@@ -1,7 +1,27 @@
 class Admin::ParsonalColorsController < ApplicationController
+  
+  def create
+     parsonal_color = ParsonalColor.new(parsonal_color_params)
+     parsonal_color.save
+     redirect_to '/admin/parsonal_colors'
+  end
   def edit
+    @parsonal_color = ParsonalColor.find(params[:id])
   end
 
+  def update
+    @parsonal_color = ParsonalColor.find(params[:id])
+    @parsonal_color.update(parsonal_color_params)
+    redirect_to '/admin/parsonal_colors'
+  end
+  
   def index
+     @parsonal_color = ParsonalColor.new
+     @parsonal_colors = ParsonalColor.all
+  end
+  
+    private
+  def parsonal_color_params
+    params.require(:parsonal_color).permit(:color_name)
   end
 end
